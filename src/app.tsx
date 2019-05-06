54769335278c1199c1150600a731c3111a5d306d
masa1118 <maksim.samokhin@netcracker.com>
2019-05-06 10:57:20 +0300

    start tau

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './app.scss';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+          Курсач по ТАУ
+      </div>
+    );
+  }
+}
+
+export default App;
